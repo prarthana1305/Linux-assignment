664463c72bcd4133d35fd5a4b05e1f369c5cf17f
prarthana1305 <150007875+prarthana1305@users.noreply.github.com>
2025-01-30 07:40:56 +0530

    Create addnumbers.sh

---
+++ assign-3 shell script/addnumbers.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Prompt user to enter the first number
+echo "Enter the first number: "
+read num1
+
+# Prompt user to enter the second number
+echo "Enter the second number: "
+read num2
+
+# Add the numbers and store the result in a third variable
+result=$((num1 + num2))
+
+# Print the result
+echo "The sum of $num1 and $num2 is: $result"
+
+
+
+
+
+
+
+
+
+
+
